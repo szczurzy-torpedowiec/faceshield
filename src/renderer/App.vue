574089ab80d38adc8cf80e6df59e087669465835
Dominik Korsa <dominik.korsa@gmail.com>
2020-10-11 11:08:05 +0200

    Add router

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -38,21 +38,14 @@
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
   data: () => ({
     //
   }),
---
+++ src/renderer/router/routes.js
@@ -0,0 +1,17 @@
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        component: About
+    }
+]
+
+export default routes
