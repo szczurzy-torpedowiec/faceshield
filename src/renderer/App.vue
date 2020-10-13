c2e9a39da9a9ae1be068cf210e8d02ca3de83e30
Dominik Korsa <dominik.korsa@gmail.com>
2020-10-13 17:38:17 +0200

    Separate navigation component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,61 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      permanent
-      :expand-on-hover="$vuetify.breakpoint.smAndDown"
-    >
-      <div class="d-flex flex-column fill-height">
-        <v-list>
-          <v-list-item to="/">
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Dashboard
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/stats">
-            <v-list-item-icon>
-              <v-icon>mdi-chart-timeline-variant</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Statistics
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/gallery">
-            <v-list-item-icon>
-              <v-icon>mdi-video</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Gallery
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-        <v-spacer />
-        <v-divider />
-        <v-list>
-          <v-list-item to="/settings">
-            <v-list-item-icon>
-              <v-icon>mdi-cog</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              Settings
-            </v-list-item-title>
-          </v-list-item>
-          <v-list-item to="/about">
-            <v-list-item-icon>
-              <v-icon>mdi-information</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>
-              About
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </div>
-    </v-navigation-drawer>
-
+    <navigation />
     <v-main>
       <v-scroll-x-transition mode="out-in">
         <router-view />
@@ -65,10 +10,9 @@
 </template>
 
 <script>
+import Navigation from "./components/Navigation";
 export default {
   name: 'App',
-  data: () => ({
-    drawer: false,
-  }),
+  components: {Navigation},
 };
 </script>
---
+++ src/renderer/components/Navigation.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-navigation-drawer
+      app
+      permanent
+      :expand-on-hover="$vuetify.breakpoint.smAndDown"
+  >
+    <div class="d-flex flex-column fill-height">
+      <v-list>
+        <v-list-item to="/">
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Dashboard
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/stats">
+          <v-list-item-icon>
+            <v-icon>mdi-chart-timeline-variant</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Statistics
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/gallery">
+          <v-list-item-icon>
+            <v-icon>mdi-video</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Gallery
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+      <v-spacer />
+      <v-divider />
+      <v-list>
+        <v-list-item to="/settings">
+          <v-list-item-icon>
+            <v-icon>mdi-cog</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            Settings
+          </v-list-item-title>
+        </v-list-item>
+        <v-list-item to="/about">
+          <v-list-item-icon>
+            <v-icon>mdi-information</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>
+            About
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </div>
+  </v-navigation-drawer>
+</template>
