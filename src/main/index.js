d77b02590f82c8901b23ada7278109ecccb01cc1
Dominik Korsa <dominik.korsa@gmail.com>
2020-10-23 21:37:09 +0200

    Fix app version in taskbar

--- src/main/index.js
+++ src/main/index.js
@@ -146,7 +146,7 @@ function createTray() {
       role: 'quit',
     },
   ]);
-  tray.setToolTip(process.env.WEBPACK_DEV_SERVER_URL ? 'Face Shield development' : `Face Shield ${process.version}`);
+  tray.setToolTip(process.env.WEBPACK_DEV_SERVER_URL ? 'Face Shield development' : `Face Shield v${app.getVersion()}`);
   tray.setContextMenu(contextMenu);
   tray.addListener('click', () => {
     if (win === null) {
